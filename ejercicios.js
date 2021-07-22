7eda8aa64181322cbaf29a40a06c39f93ab5fecf
unknown <zuleta-96@hotmail.com>
2021-07-21 21:06:36 -0500

    Agregar ejercicio 1

--- ejercicios.js
+++ ejercicios.js
@@ -7,6 +7,14 @@ app.use(express.json());
 
 const port = 3000;
 
+// 1.	Agrega un endpoint '/api/' que responda a una petición de tipo GET con un código de estado 200 y el siguiente json:
+//             { 'mensaje':'hola mundo' }
+
+app.get("/api", (req, res) => {
+  res.status(200).send({ mensaje: "Hola Mundo" });
+});
+
+
 // 2.	Agrega un endpoint '/api/suma' que responda a una petición de tipo GET con la suma de dos números que reciba mediante las querys num1 y num2.
 // El servidor debe responder con un código de estado 200 y un json como el siguiente: { 'resultado': 7 }
 
